5b54f0e0905d7bb9bfa5122066a2ed9946d48ff1
Daniel Miller <daniel.miller@sainsburys.co.uk>
2020-06-23 09:28:14 +0100

    code ready for submission

--- page_rank.py
+++ page_rank.py
@@ -11,6 +11,15 @@ SITE_PROBABILITY_MATRIX = np.array([[0,   1/2, 1/3, 0, 0,   0 ],
               [0,   0,   1/3, 0, 0,   0 ]]
               )
 
+SELF_LOOP_ARRAY = np.array(
+    [[0,   1/2, 1/3, 0, 0,   0, 0 ],
+     [1/3, 0,   0,   0, 1/2, 0, 0 ], 
+     [1/3, 1/2, 0,   1, 0,   0, 0 ],
+     [1/3, 0,   1/3, 0, 1/2, 0, 0 ],
+     [0,   0,   0,   0, 0,   0, 0 ],
+     [0,   0,   1/3, 0, 0,   0, 0 ],
+     [0,   0,   0,   0, 0,   1, 1]]
+)
 def calculate_page_rank_brute_force(site_probability_matrix: np.array, count=100) -> np.array:
 
     base_site_land_vector = np.ones(site_probability_matrix.shape[0]) / site_probability_matrix.shape[0]
@@ -30,3 +39,45 @@ def calculate_page_rank_eigen_decomposition(site_probability_matrix: np.array, c
     transformation_matrix = eigenvectors @ result
     return transformation_matrix @ base_site_land_vector
 
+
+def eigen_dampen(site_probability_matrix: np.array, count=100, dampen_parameter=0.3) -> np.array:
+    site_probability_matrix = add_dampen_parameter(site_probability_matrix, dampen_parameter, site_probability_matrix.shape[0])
+    
+    base_site_land_vector = np.ones(site_probability_matrix.shape[0]) / site_probability_matrix.shape[0]
+    eigenvalues, eigenvectors = la.eig(site_probability_matrix)
+    diagonal_eigenvector_power = np.diag(eigenvalues) ** count
+    result = diagonal_eigenvector_power @ la.inv(eigenvectors)
+    transformation_matrix = eigenvectors @ result
+    return transformation_matrix @ base_site_land_vector
+
+def calculate_page_rank_brute_force(site_probability_matrix: np.array, count=47) -> np.array:
+
+    base_site_land_vector = np.ones(site_probability_matrix.shape[0]) / site_probability_matrix.shape[0]
+    current_site_land_vector = site_probability_matrix @ base_site_land_vector
+    i = 0
+    while (i < count):
+        base_site_land_vector = current_site_land_vector
+        current_site_land_vector = site_probability_matrix @ current_site_land_vector
+        i+=1
+    return current_site_land_vector, base_site_land_vector
+
+def calculate_brute_force_dampen(site_probability_matrix: np.array, count=100, dampen_parameter=0.3) -> np.array:
+
+    site_probability_matrix = add_dampen_parameter(site_probability_matrix, dampen_parameter, site_probability_matrix.shape[0])
+
+    base_site_land_vector = np.ones(site_probability_matrix.shape[0]) / site_probability_matrix.shape[0]
+    current_site_land_vector = site_probability_matrix @ base_site_land_vector
+    i = 0
+    while (i < count):
+        base_site_land_vector = current_site_land_vector
+        current_site_land_vector = site_probability_matrix @ current_site_land_vector
+        i+=1
+    return current_site_land_vector, base_site_land_vector
+
+
+def add_dampen_parameter(site_probability_matrix: np.array, dampen_parameter: int, shape: int) -> np.array:
+    site_probability_matrix *= dampen_parameter
+    one_matrix = np.ones((shape, shape)) * (1 - dampen_parameter) / shape
+    return site_probability_matrix + one_matrix
+
+    
--- tests/test_page_rank.py
+++ tests/test_page_rank.py
@@ -1,25 +1,37 @@
-from page_rank import calculate_page_rank_brute_force, calculate_page_rank_eigen_decomposition
+import page_rank
 import numpy as np
 import numpy.linalg as la
-from page_rank import SITE_PROBABILITY_MATRIX
 import unittest
+import copy
 
 class TestPageRank(unittest.TestCase):
 
     def test_page_rank_on_simple_probability_matrix(self):
-        site_probability_vector, previous_site_vector = calculate_page_rank_brute_force(
-            np.array(SITE_PROBABILITY_MATRIX)
+        site_probability_vector, previous_site_vector = page_rank.calculate_page_rank_brute_force(
+            np.array(page_rank.SITE_PROBABILITY_MATRIX)
         )
         self.assertAlmostEqual(np.sum(site_probability_vector), 1)
         self.assertLessEqual(la.norm(site_probability_vector - previous_site_vector), 0.01)
     
     def test_eigenvector_decomposition(self):
         # proof M**N*V = IE**N*I^-1V
-        eigen_probability_vector = calculate_page_rank_eigen_decomposition(
-            np.array(SITE_PROBABILITY_MATRIX)
+        eigen_probability_vector = page_rank.calculate_page_rank_eigen_decomposition(
+            np.array(page_rank.SITE_PROBABILITY_MATRIX)
         )
-        site_probability_vector, previous_site_vector = calculate_page_rank_brute_force(
-            np.array(SITE_PROBABILITY_MATRIX)
+        site_probability_vector, previous_site_vector = page_rank.calculate_page_rank_brute_force(
+            np.array(page_rank.SITE_PROBABILITY_MATRIX)
         )
 
         self.assertAlmostEqual(la.norm(eigen_probability_vector), la.norm(site_probability_vector))
+    
+    def test_brute_force_with_damping_parameter(self):
+        current, base = page_rank.calculate_brute_force_dampen(np.array(page_rank.SELF_LOOP_ARRAY))
+        self.assertAlmostEqual(np.sum(current), 1)
+    
+    def test_eigen_decomposition_with_damping_parameter(self):
+        current, base = page_rank.calculate_brute_force_dampen(np.array(page_rank.SELF_LOOP_ARRAY))
+        dampen_vector = page_rank.eigen_dampen(np.array(page_rank.SELF_LOOP_ARRAY))
+        dampen_vector_sum = np.sum(dampen_vector)
+        self.assertAlmostEqual(float(dampen_vector_sum), 1)
+        for i in range(0, dampen_vector.shape[0]):
+            self.assertAlmostEqual(current[i], dampen_vector[i])
